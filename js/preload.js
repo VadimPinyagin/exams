5005af3c54ca44bd1c75147f8692a2379ae8deeb
Waterfall <vadimbox85@gmail.com>
2024-02-03 09:49:19 +0300

    creation

---
+++ js/preload.js
@@ -0,0 +1,40 @@
+// массив изобажений нсрц
+img_nsrc= new Array();
+img[0]=new Image();   // Каска НСРЦ
+img[1]=new Image();   // Каска НСРЦ со свечением
+img[2]=new Image();   // Каска НСРЦ
+ 
+//сами изображения.
+img[0].src="img/helments/yellow_helmet_nsrc.png"
+img[1].src="img/helments/helment_light/yellow_helmet_nsrc_light.png"
+img[2].src="img/helments/yellow_helmet_nsrc.png"
+
+img_vies= new Array();
+img[3]=new Image();   // Каска ВИЭС
+img[4]=new Image();   // Каска ВИЭС со свечением
+img[5]=new Image();   // Каска ВИЭС
+ 
+//сами изображения.
+img[3].src="img/helments/yellow_helmet_vies.png"
+img[4].src="img/helments/helment_light/yellow_helmet_vies_light.png"
+img[5].src="img/helments/yellow_helmet_vies.png"
+
+img_sottoro= new Array();
+img[6]=new Image();   // Каска СОТТОРО
+img[7]=new Image();   // Каска СОТТОРО со свечением
+img[8]=new Image();   // Каска СОТТОРО
+ 
+//сами изображения.
+img[6].src="img/helments/yellow_helmet_sottoro.png"
+img[7].src="img/helments/helment_light/yellow_helmet_sottoro_light.png"
+img[8].src="img/helments/yellow_helmet_sottoro.png"
+
+img_ottoro= new Array();
+img[9]=new Image();   // Каска ОТТО
+img[10]=new Image();   // Каска ОТТО со свечением
+img[11]=new Image();   // Каска ОТТО
+ 
+//сами изображения.
+img[12].src="img/helments/yellow_helmet_ottoro.png"
+img[13].src="img/helments/helment_light/yellow_helmet_ottoro_light.png"
+img[14].src="img/helments/yellow_helmet_ottoro.png"
